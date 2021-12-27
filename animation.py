926ff807e6dc507768945317e32a3c2c04d3e866
TanJQY <91547716+TanJQY@users.noreply.github.com>
2021-12-27 16:53:23 -0500

    Add files via upload

--- animation.py
+++ animation.py
@@ -8,6 +8,11 @@ import random
 window_size = (800, 650)
 game_display = pygame.display.set_mode(window_size)
 
+#declares variables for text and opens text file
+dialogues = open("dialogues.txt","r")
+part1_line = dialogues.readline()
+part2_line1 = dialogues.readline()
+part2_line2 = dialogues.readline()
 
 #loads images
 instructions_bg = pygame.image.load("instructions.png")
@@ -16,11 +21,22 @@ hallway_bg = pygame.image.load("hallway4.jpg")
 hallway_bg = pygame.transform.scale(hallway_bg, window_size)
 hachiko_neutral = pygame.image.load("hachiko.png")
 hachiko_neutral = pygame.transform.scale(hachiko_neutral, (280, 372))
+
 #defines some colours
 white = (255, 255, 255)
 black = (0, 0, 0)
 off_white = (239, 222, 205)
 pink = (255, 0, 85)
+darker_pink = (255, 0, 50)
+
+#defines some variables for displaying dialogue
+dialogue_box_x = 50
+dialogue_box_y = 445
+dialogue_box_length = 700
+dialogue_box_height = 200
+
+#defines variables used for goodwill meter
+filled_amount = int(50)
 
 #defines some variables for displaying text
 font_file = "PlayfairDisplayRegular.ttf"
@@ -38,103 +54,108 @@ def text_rect(text):
     return text_rect
 
 
+
 #draws buttons, directs user to different sections depending on the button pressed
-def button(msg, x, y, length, height, border, border_colour, button_colour, action, font_size, font_colour):
+#goodwill paramater needed to pass on information about the goodwill meter from part to part
+def button(msg, x, y, length, height, border, border_colour, button_colour, action, font_size, font_colour, goodwill):
     
     #draws button
     pygame.draw.rect(game_display, button_colour, (x, y, length, height), width = 0, border_radius = 30)
     
+    #defines some variables related to the mouse
     mouse_pos = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     
     #if the mouse is hovering on the button draw a border 
     if x + length > mouse_pos[0] > x and y + height > mouse_pos[1] > y:
         pygame.draw.rect(game_display, border_colour, (x, y, length, height), width = border, border_radius = 30)
+        
         if click[0] == 1:
-            if action == "prologue":
-                prologue()
-                
+            #checks and plays which part should happen next
             if action == "part 1":
                 part_1()
                 
             if action == "part 2":
-                part_2()
+                part_2(goodwill)
            
             if action == "part 3 a":
-                part_3("a")
+                part_3("a", goodwill)
            
             if action == "part 3 b":
-                part_3("b")
+                part_3("b", goodwill)
             
             if action == "part 4":
-                part_4()
+                part_4(goodwill)
             
             if action == "part 5 a":
-                part_5("a")
+                part_5("a", goodwill)
                 
             if action == "part 5 b":
-                part_5("b")
+                part_5("b", goodwill)
                 
             if action == "part 5 c":
-                part_5("c")
+                part_5("c", goodwill)
                 
             if action == "part 6":
-                part_6()
+                part_6(goodwill)
             
             if action == "part 7 a":
-                part_7("a")
+                part_7("a", goodwill)
             
             if action == "part 7 b":
-                part_7("b")
+                part_7("b", goodwill)
             
             if action == "part 8 a":
-                part_8("a")
+                part_8("a", goodwill)
             
             if action == "part 8 b":
-                part_8("b")
+                part_8("b", goodwill)
             
             if action == "part 9":
-                part_9()
+                part_9(goodwill)
             
             if action == "part 10":
-                part_10()
+                part_10(goodwill)
             
             if action == "part 11":
-                part_11()
+                part_11(goodwill)
             
             if action == "part 12 a":
-                part_12("a")
+                part_12("a", goodwill)
                 
             if action == "part 12 b":
-                part_12("b")
+                part_12("b", goodwill)
                 
             if action == "part 12 c":
-                part_12("c")
+                part_12("c", goodwill)
                 
             if action == "part 12 d":
-                part_12("d")
+                part_12("d", goodwill)
             
             if action == "part 13":
-                part_13()
+                part_13(goodwill)
             
             if action == "part 14":
-                part_14()
+                part_14(goodwill)
                 
             if action == "ending 1":
-                ending_1()
+                ending_1(goodwill)
             
             if action == "ending 2":
-                ending_2()
+                ending_2(goodwill)
             
             if action == "ending 3":
-                ending_3()
+                ending_3(goodwill)
             
-  
-    #draws text on button    
+      
+    #renders text
     text = render_text(msg, font_file, font_colour, font_size)
-    #Centers the text on the button
+    
+    #centers the text on the button
     text_pos = text_rect(text)
     text_pos.center = ( (x + (length/2)), (y + (height/2)))
+    
+    #blits text on the button
     game_display.blit(text, text_pos)
 
 
@@ -148,109 +169,202 @@ def random_num(probability):
     else:
         return("outcome 2")
     
+    
 #draws dialogue box
 def dialogue_box():
-    box_x = 50
-    box_y = 425
-    box_length = 700
-    box_height = 200
+    #draws box
+    pygame.draw.rect(game_display, off_white, (dialogue_box_x, dialogue_box_y, dialogue_box_length, dialogue_box_height), border_radius = 30)
+    #draws border
+    pygame.draw.rect(game_display, pink, (dialogue_box_x, dialogue_box_y, dialogue_box_length, dialogue_box_height), width = 3, border_radius = 30)
+
+
+#displays the dialogue in the dialogue box
+def display_dialogue(msg):
     
-    pygame.draw.rect(game_display, off_white, (box_x, box_y, box_length, box_height), border_radius = 30)
-    pygame.draw.rect(game_display, pink, (box_x, box_y, box_length, box_height), width = 3, border_radius = 30)
+    #max words per line, used to indicate when a new line should be made
+    max_words = 13
     
+    #variables for the position of text
+    x = dialogue_box_x + (dialogue_box_length/2)
+    y = dialogue_box_y + 30
+    
+    #separates the dialogue into words
+    words = msg.split()
+    
+    line = ""
+    
+    #finds how many lines there should be
+    num_of_lines = len(words) // max_words
+    #adds on an extra line for extra words
+    if len(words) % max_words != 0:
+        num_of_lines += 1
+    
+    for i in range(num_of_lines):
+        for j in range(max_words): 
+        #adds words to the current line and pops them out of the words list
+            line += " " + words[0]
+            words.pop(0)
+            if len(words) == 0:
+                break
+            
+        #displays the line of text    
+        text = render_text(line, font_file, pink, 18)
+        text_pos = text_rect(text)
+        text_pos.center = (x , y)
+        game_display.blit(text, text_pos)
+        
+        #updates y for the position of the next line, resets the words for the next line
+        y += 25
+        line = "" 
+        
+
+#displays a label for who is speaking
 def speaker_box(who):
     x = 50
-    y = 375
+    y = 395
     height = 40
-    if who == "hachiko":
-        length = 115
+    length = int()
     
+    #adjusts length depending on the name
+    if who == "Hachiko":
+        length = 115
+    if who == "You":
+        length = 75
+    
+    #draws rectangle and border
     pygame.draw.rect(game_display, off_white, (x, y, length, height), border_radius = 12)
     pygame.draw.rect(game_display, pink, (x, y, length, height), width = 3, border_radius = 12)
-     
+    
+    #displays centered text 
     text = render_text(who, font_file, pink, 20)
     text_pos = text_rect(text)
     text_pos.center = ( (x + (length/2)), (y + (height/2)))
     game_display.blit(text, text_pos)
-        
+
+#displays the goodwill meter        
 def goodwill_meter(filled):
+    
+    #variables for the position of goodwill meter
     x = 550
     y = 50
     length = 200
     height = 20
-    pygame.draw.rect(game_display, black, (x, y, length, height), border_radius = 30)
-    text = render_text("goodwill: ", font_file, white, 18)
-    game_display.blit(text, (450, 45))    
     
-    pygame.draw.rect(game_display, white, (x, y, filled, height), border_radius = 30)
+    #variables for the position of the text
+    text_x = int()
+    text_y = y - 10
+    
+    #adjusts position of text depending on the length of the value
+    if len(str(filled)) == 1:
+        text_x = x - 100
+    if len(str(filled)) == 2:
+        text_x = x - 120
+    
+    #displays text
+    text = "Goodwill: " + str(filled)
+    text = render_text(text, font_file, white, 18)
+    game_display.blit(text, (text_x, text_y))    
+    
+    #draws goodwill meter
+    pygame.draw.rect(game_display, black, (x, y, length, height), border_radius = 30)    
+    #draws filled portion
+    #filled portion is mutliplied by 2 since 1% of the bar is 2 pixels
+    pygame.draw.rect(game_display, white, (x, y, (filled*2), height), border_radius = 30)
+    #draws border
     pygame.draw.rect(game_display, pink, (x, y, length, height), width = 2, border_radius = 30)
 
 
+#code for the instructions section
 def instructions():
     running = True
     while running:
-        
-        #blit background
+        #displays background
+        game_display.fill(white)
         game_display.blit(instructions_bg, (0, 0))
         
-        button("next", 325, 570, 150, 50, 2, pink, off_white, "prologue", 20, black)
+        #displays some text
+        text = render_text("click anywhere to continue", font_file, black, 24)
+        game_display.blit(text, (250, 575))
+        
+        
+        #button("next", 325, 570, 150, 50, 2, pink, off_white, "prologue", 20, black)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            
+            if pygame.mouse.get_pressed() == (1, 0, 0):
+                prologue()
+        
         pygame.display.update()
 
-#defines prologue section
+#code for the prologue section
 def prologue():
     running = True
     while running:
+        game_display.fill(white)
         game_display.blit(prologue_bg, (0, 0))
        
-        button("next", 425, 550, 150, 50, 2, pink, off_white, "part 1", 20, black)
-
+        #button("next", 425, 400, 150, 50, 2, pink, off_white, "part 1", 20, black)
+        text = render_text("click anywhere to continue", font_file, black, 24)
+        game_display.blit(text, (370, 575))        
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if pygame.mouse.get_pressed() == (1, 0, 0):
+                part_1()            
         
         pygame.display.update()
 
 def part_1():
     running = True
     while running:
+        game_display.fill(white)
         game_display.blit(hallway_bg, (0, 0))
 
         game_display.blit(hachiko_neutral, (100, 100))
         dialogue_box()
-        speaker_box("hachiko")
-        goodwill_meter(100)
-     
-        text = render_text("part 1", font_file, white, font_size)
-        game_display.blit(text, (50, 50))
+        speaker_box("Hachiko")
         
-
-        button("yes", 400, 300, 100, 75, 1, white, (0, 0, 255), "part 2", 20, black)
-        button("no", 300, 300, 100, 75, 1, white, (0, 0, 255), "ending 1", 20, black)        
+        goodwill_meter(filled_amount)
+        
+        display_dialogue(part1_line)
+        
+        mouse_pos = pygame.mouse.get_pos()
+        
+        button("Yes, I am.", 75, 570, 200, 50, 2, black, pink, "part 2", 18, white, filled_amount)
+        button("Sorry, I think you have the wrong person.", 315, 570, 375, 50, 2, black, pink, "ending 1", 18, white, filled_amount)        
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+                                        
+                                    
         
         pygame.display.update()
     
-def part_2():
+def part_2(goodwill):
     running = True
+    filled_amount = goodwill 
+    filled_amount += random.randint(1, 5)
+    
     while running:
-        game_display.fill(black)
-        text = render_text("part 2", font_file, white, font_size)
-        game_display.blit(text, (50, 50))
+        game_display.fill(white)
+        game_display.blit(hallway_bg, (0, 0))
         
+        game_display.blit(hachiko_neutral, (100, 100))
+        dialogue_box()
+        speaker_box("You")
+        display_dialogue(part2_line1)
+        goodwill_meter(filled_amount)
+        
+        if pygame.mouse.get_pressed() == (1, 0, 0):
+            
+            part_2_1(goodwill)
 
-        button("escape", 500, 500, 100, 75, 1, white, (0, 0, 255), "part 3 a", 20, black)
-        button("obedience", 200, 500, 100, 75, 1, white, (0, 0, 255), "part 3 b", 20, black)        
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -258,8 +372,29 @@ def part_2():
         
         pygame.display.update()
 
+def part_2_1(goodwill):
+    running = True
+    while running:
+        game_display.fill(white)
+        game_display.blit(hallway_bg, (0, 0))
+        
 
-def part_3(part):
+        game_display.blit(hachiko_neutral, (100, 100))                
+        dialogue_box()
+        speaker_box("Hachiko")
+        display_dialogue(part2_line2)
+        
+        button("escape", 500, 500, 100, 75, 1, white, (0, 0, 255), "part 3 a", 20, black, goodwill)
+        button("obedience", 200, 500, 100, 75, 1, white, (0, 0, 255), "part 3 b", 20, black, goodwill)        
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        pygame.display.update()
+        
+def part_3(part, goodwill):
     running = True
     while running:
         if part == "a":
@@ -288,7 +423,7 @@ def part_3(part):
         
         pygame.display.update()
         
-def part_4():
+def part_4(goodwill):
     running = True
     while running:
         game_display.fill(black)
@@ -307,7 +442,7 @@ def part_4():
         pygame.display.update()
 
 #defines instructions section
-def part_5(part):
+def part_5(part, goodwill):
     running = True
     while running:
         
@@ -324,7 +459,7 @@ def part_5(part):
         pygame.display.update()
 
 
-def part_6():
+def part_6(goodwill):
     running = True
     while running:
         
@@ -342,7 +477,7 @@ def part_6():
                 quit()
         pygame.display.update()
 
-def part_7(part):
+def part_7(part, goodwill):
     running = True
     while running:
         if part == "a":
@@ -370,7 +505,7 @@ def part_7(part):
         pygame.display.update()
 
 
-def part_8(part):
+def part_8(part, goodwill):
     running = True
     while running:
         if part == "a":
@@ -395,7 +530,7 @@ def part_8(part):
                 quit()
         pygame.display.update()
 
-def part_9():
+def part_9(goodwill):
     running = True
     while running:
         game_display.fill(black)
@@ -414,7 +549,7 @@ def part_9():
         pygame.display.update()
 
 
-def part_10():
+def part_10(goodwill):
     running = True
     while running:
         
@@ -430,7 +565,7 @@ def part_10():
                 quit()
         pygame.display.update()
 
-def part_11():
+def part_11(goodwill):
     running = True
     while running:
         action = ""
@@ -457,7 +592,7 @@ def part_11():
                 quit()
         pygame.display.update()
 
-def part_12(part):
+def part_12(part, goodwill):
     running = True
     while running:
         
@@ -474,7 +609,7 @@ def part_12(part):
         pygame.display.update()
 
 
-def part_13():
+def part_13(goodwill):
     running = True
     while running:
         
@@ -490,7 +625,7 @@ def part_13():
                 quit()
         pygame.display.update()
 
-def part_14():
+def part_14(goodwill):
     running = True
     while running:
         
@@ -507,7 +642,7 @@ def part_14():
         pygame.display.update()
 
 
-def ending_1():
+def ending_1(goodwill):
     running = True
     while running:
         game_display.fill(black)
@@ -526,7 +661,7 @@ def ending_1():
         pygame.display.update()    
 
 
-def ending_2():
+def ending_2(goodwill):
     running = True
     while running:
         game_display.fill(black)
@@ -545,7 +680,7 @@ def ending_2():
         pygame.display.update()    
 
 
-def ending_3():
+def ending_3(goodwill):
     running = True
     while running:
         game_display.fill(black)
